5e8bcce3409430b01937575527c13dbb2e7e9f49
jayferreira <jayzoca@gmail.com>
2019-01-27 18:46:00 -0200

    Inclusão da primeira versão que funcionou da cifra, alterações no HTML

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,18 @@
-window.cipher = {
-  // ... 
-};
+function cifra() {
+  var input = "CAR";
+  var i;
+  var numeroASC; 
+  var desloc = 3;
+  var resultASC;
+  var palavraCodificada = [];
+  var palavraComDeslocamento = [];
+  for (i = 0; i < input.length; i++) { 
+    numeroASC = input.charCodeAt(i)
+    resultASC = ( numeroASC - 65 + desloc ) % 26 + 65
+    palavraComDeslocamento.push(String.fromCharCode(resultASC))
+    
+    
+  }
+  return alert(palavraComDeslocamento);
 
-
-mexi aaqui pra testar 
+}
