053a59461ff9fc04f7c075ba8899f16635203c12
jayferreira <jayzoca@gmail.com>
2019-01-30 20:16:01 -0200

    inclusão do mod para melhor funcionamento da cifra

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,8 @@
+function mod(n, m) {
+  return ((n % m) + m) % m;
+}
+
+
 function encode(desloc, input) {
   
   let numeroASC = 0;
@@ -8,11 +13,11 @@ function encode(desloc, input) {
     }
     else {
       if (input[i].charCodeAt() >= 65 && input[i].charCodeAt() <= 90) {
-        numeroASC = ((input[i].charCodeAt() - 65 + desloc) % 26) + 65;
+        numeroASC = mod((input[i].charCodeAt() - 65 + desloc), 26) + 65;
         finalString += String.fromCharCode(numeroASC);
       }
       else if (input[i].charCodeAt() >= 97 && input[i].charCodeAt() <= 122) {
-        numeroASC = ((input[i].charCodeAt() - 97 + desloc) % 26) + 97;
+        numeroASC = mod((input[i].charCodeAt() - 97 + desloc), 26) + 97;
         finalString += String.fromCharCode(numeroASC);
       }
     }
@@ -42,11 +47,11 @@ function decode(desloc, input) {
     }
     else {
       if (input[i].charCodeAt() >= 65 && input[i].charCodeAt() <= 90) {
-        numeroASC = ((input[i].charCodeAt() - 65 + (desloc * -1 )) % 26) + 65;
+        numeroASC = mod((input[i].charCodeAt() - 65 + (desloc * -1 )), 26) + 65;
         finalString += String.fromCharCode(numeroASC);
       }
       else if (input[i].charCodeAt() >= 97 && input[i].charCodeAt() <= 122) {
-        numeroASC = ((input[i].charCodeAt() - 97 + (desloc * -1 )) % 26) + 97;
+        numeroASC = mod((input[i].charCodeAt() - 97 + (desloc * -1 )), 26) + 97;
         finalString += String.fromCharCode(numeroASC);
       }
     }
