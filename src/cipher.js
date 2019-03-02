8fcff92a2f32f12e794c29cf57aead187e68c69d
jayferreira <jayzoca@gmail.com>
2019-03-02 19:52:41 -0300

    inclusão do botão de copiar - inclusão de HTML semântico

--- src/cipher.js
+++ src/cipher.js
@@ -4,7 +4,6 @@ function mod(n, m) {
 
 
 function encode(offset, message) {
-  
   let asciiCode = 0;
   let finalString = "";
   for (let i = 0; i < message.length; i++) {
@@ -22,22 +21,24 @@ function encode(offset, message) {
     else {
       finalString += message[i];
     }
-    
   }
   return finalString;
-} 
+}
 
 function getText() {
   const offset = parseInt(document.getElementById("key").value);
-  let message = document.getElementById("userInput");
+  let message = document.getElementById("user-input");
   const final = encode(offset, message.value);
   message.value = final;
-  message.select();
-  message = document.execCommand("copy");
+}
+
+function copyText() { 
+  let copyBtn = document.getElementById("user-input")
+  copyBtn.select();
+  document.execCommand("copy");
 }
 
 function decode(offset, message) {
-  
   let asciiCode = 0;
   let finalString = "";
   for (let i = 0; i < message.length; i++) {
@@ -45,11 +46,11 @@ function decode(offset, message) {
       finalString += " ";
     }
     else if (message[i].charCodeAt() >= 65 && message[i].charCodeAt() <= 90) {
-      asciiCode = mod((message[i].charCodeAt() - 65 + (offset * -1 )), 26) + 65;
+      asciiCode = mod((message[i].charCodeAt() - 65 + (offset * -1)), 26) + 65;
       finalString += String.fromCharCode(asciiCode);
     }
     else if (message[i].charCodeAt() >= 97 && message[i].charCodeAt() <= 122) {
-      asciiCode = mod((message[i].charCodeAt() - 97 + (offset * -1 )), 26) + 97;
+      asciiCode = mod((message[i].charCodeAt() - 97 + (offset * -1)), 26) + 97;
       finalString += String.fromCharCode(asciiCode);
     }
     else {
@@ -58,15 +59,16 @@ function decode(offset, message) {
   }
   return finalString;
 }
-  
-  
 
 function getText2() {
   const offset = parseInt(document.getElementById("keyD").value);
-  let message = document.getElementById("userInputD");
+  let message = document.getElementById("user-input-decode");
   const final = decode(offset, message.value);
-
   message.value = final;
-  message.select();
-  message = document.execCommand("copy");
+}
+
+function copyText2() { 
+  let copyBtn2 = document.getElementById("user-input-decode")
+  copyBtn2.select();
+  document.execCommand("copy");
 }
--- src/tests.js
+++ src/tests.js
@@ -1,38 +1,82 @@
-//início dos testes de encode
+//start of the encoding tests
 let finalTest1 = encode(3, 'car');
 if (finalTest1 === 'fdu') {
-	console.log("funcionou certinho!! O resultado " + finalTest1 + " é o esperado."); 
+	console.log("Passou! O resultado " + finalTest1 + " é o esperado."); 
 } else {
 	console.log("tentar de novo! O resultado era pra ser 'fdu', mas foi" + finalTest1 + ". You can do it");
 }
 
 let finalTest2 = encode(7, 'ABACATE');
 if (finalTest2 === 'HIHJHAL') {
-	console.log("funcionou certinho!! O resultado 'HIHJHAL' é o esperado."); 
+	console.log("Passou! O resultado 'HIHJHAL' é o esperado."); 
 } else {
 	console.log("tentar de novo! O resultado era pra ser:'HIHJHAL', mas foi" + finalTest2 + ". You can do it");
 }
 
 let finalTest3 = encode(3, 'ola tudo bem');
 if (finalTest3 === 'rod wxgr ehp') {
-	console.log("funcionou certinho!! O resultado 'rod wxgr ehp' é o esperado."); 
+	console.log("Passou! O resultado 'rod wxgr ehp' é o esperado."); 
 } else {
 	console.log("tentar de novo! O resultado era pra ser:'rod wxgr ehp', mas foi" + finalTest3 + ".You can do it");
 }
 
-//início dos testes de decode 
+let finalTest4 = encode(500, 'The Quick Brown Fox Jumps Over The Lazy Dog.');
+if (finalTest4 === 'Znk Waoiq Hxuct Lud Pasvy Ubkx Znk Rgfe Jum.') {
+	console.log("Passou! O resultado 'Znk Waoiq Hxuct Lud Pasvy Ubkx Znk Rgfe Jum.' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser:'Znk Waoiq Hxuct Lud Pasvy Ubkx Znk Rgfe Jum.', mas foi" + finalTest4 + ".You can do it");
+}
 
+//start of the decoding tests 
 let decTest1 = decode(3, 'fdu');
 if (decTest1 === 'car') {
-	console.log("funcionou certinho!! O resultado 'car' é o esperado."); 
+	console.log("Passou! O resultado 'car' é o esperado."); 
 } else {
 	console.log("tentar de novo! O resultado era pra ser:'car', mas foi" + decTest1 + ".You can do it");
 }
 
 let decTest2 = decode(7, 'HIHJHAL');
 if (decTest2 === 'ABACATE') {
-	console.log("funcionou certinho!! O resultado 'ABACATE' é o esperado."); 
+	console.log("Passou! O resultado 'ABACATE' é o esperado."); 
 } else {
 	console.log("tentar de novo! O resultado era pra ser:'ABACATE', mas foi" + decTest2 + ". You can do it");
 }
 
+let decTest3 = decode(500, 'Znk Waoiq Hxuct Lud Pasvy Ubkx Znk Rgfe Jum.');
+if (decTest3 === 'The Quick Brown Fox Jumps Over The Lazy Dog.') {
+	console.log("Passou! O resultado 'The Quick Brown Fox Jumps Over The Lazy Dog.' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser:'The Quick Brown Fox Jumps Over The Lazy Dog.', mas foi" + decTest3 + ". You can do it");
+}
+
+
+
+//start of the tests with special characters
+let specialTest1 = encode(200, '!@#$%¨&*()_+');
+if (specialTest1 === '!@#$%¨&*()_+') {
+	console.log("Passou! O resultado '!@#$%¨&*()_+' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser:'!@#$%¨&*()_+', mas foi" + specialTest1 + ".You can do it");
+}
+
+let specialTest2 = decode(900, '!@#$%¨&*()_+');
+if (specialTest2 === '!@#$%¨&*()_+') {
+	console.log("Passou! O resultado '!@#$%¨&*()_+' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser:'!@#$%¨&*()_+', mas foi" + specialTest2 + ". You can do it");
+}
+
+//start of the test with negative offset
+let negativeTest1 = encode(-50, 'Olá, tudo bem? ');
+if (negativeTest1 === 'Qná, vwfq dgo? ') {
+	console.log("Passou! O resultado 'Qná, vwfq dgo?' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser 'Qná, vwfq dgo? ', mas foi" + negativeTest1 + ". You can do it");
+}
+
+let negativeTest2 = decode(-50, 'Qná, vwfq dgo?');
+if (negativeTest2 === 'Olá, tudo bem?') {
+	console.log("Passou! O resultado 'Olá, tudo bem?' é o esperado."); 
+} else {
+	console.log("tentar de novo! O resultado era pra ser:'Olá, tudo bem?', mas foi" + negativeTest2 + ". You can do it");
+}
